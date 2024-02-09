9425a842069ab97997c70b7511a71c158eb35d4e
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-08 20:38:39 -0600

    request working

--- src/controllers/dialogRequest.js
+++ src/controllers/dialogRequest.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const { OpenAI } = require("openai");
 const axios = require("axios");
+const max_tokens = 70;
 
 const info = require("../utils/info.js");
 const prompt = (question) => {
-  return `de la siguiente información ${info}, responde a esta pregunta ${question} contesta en primera persona y en menos de 60 caracteres.
+  return `analiza la siguiente información: ${info} y responde a esta pregunta ${question}. Contesta en primera persona y en menos de ${max_tokens} caracteres.
   
   Ten en cuenta que la pregunta está destinada al dueño de la información, si no puedes responder contesta una de estas frases: 1)'Oye! esa es información privada!', 2) 'No creo que quieras saber eso' o 3)'Tal vez si lo preguntas de otra forma...
   `;
@@ -17,19 +18,15 @@ const openai = new OpenAI({
 const dialogRequest = async (req, res) => {
   try {
     const { question } = req.body;
-    const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "system",
-          content:
-            "eres Sergio 'Cheko' García un desarrollador web muy creativo",
-        },
-        { role: "user", content: prompt(question) },
-      ],
-      model: "gpt-3.5-turbo",
+    console.log(prompt(question));
+    const completion = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: prompt(question),
+      max_tokens: max_tokens,
     });
-    res.json({ answer: completion.choices[0] });
+    res.json({ answer: completion.choices[0].text });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
