f13a6dfa0b76b940e2219a2b907df95b608c6b4f
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-02 12:49:37 -0600

    change of openAi model

--- src/controllers/dialogRequest.js
+++ src/controllers/dialogRequest.js
@@ -5,8 +5,13 @@ const max_tokens = 200;
 
 const info = require("../utils/info.js");
 const prompt = (question) => {
-  return `Analiza la siguiente información: ${info} y responde a esta pregunta en primera persona: ${question}
-- Tu respuesta debe tener menos de ${max_tokens} caracteres en total.`;
+  return `Analiza la siguiente información: ${info} y responde como si fueras un programador y solo pudieras responder con código sin ninguna explicación: ${question}
+
+tu respuesta debe ser un JSON válido con  2 propiedades:
+
+  -answer: aquí va la respuesta a la pregunta en texto plano en primera persona tener menos de ${max_tokens} caracteres en total e incluir 1 emoji
+  -category: categoría que mejor se acomode de las siguientes "personal","profesional","academic"
+  `;
 };
 
 const openai = new OpenAI({
@@ -17,37 +22,43 @@ const dialogRequest = async (req, res) => {
   try {
     const { question } = req.body;
 
-    const completion = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      prompt: prompt(question),
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      response_format: { type: "json_object" },
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: prompt(question),
+        },
+      ],
       max_tokens: max_tokens,
     });
 
-    console.log(completion);
+    console.log(completion.choices[0].message);
+
     const answer = [];
-    const cleaned = completion.choices[0].text.replace(/\n/g, "");
-    console.log(cleaned);
-    const words = cleaned.split(" ");
+    const cleaned = completion.choices[0].message.content.replace(/\n/g, "");
+    jsonResponse = JSON.parse(cleaned);
+    const words = jsonResponse.answer.split(" ");
     let tempPhrase = "";
-    let category = "nada";
+    let category = jsonResponse.category;
     for (const word of words) {
-      if (word === words[words.length - 1]) {
-        console.log("la categoría es:", word);
-        category = word.toLowerCase;
+      if ((tempPhrase + word).length <= 70) {
+        tempPhrase += (tempPhrase ? " " : "") + word;
       } else {
-        if ((tempPhrase + word).length <= 70) {
-          tempPhrase += (tempPhrase ? " " : "") + word;
-        } else {
-          answer.push(tempPhrase);
-          tempPhrase = word;
-        }
+        answer.push(tempPhrase);
+        tempPhrase = word;
       }
     }
 
     if (tempPhrase) {
       answer.push(tempPhrase);
     }
-    res.json({ answer, category: "hola" });
+    res.json({ answer, category });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "chat-gpt error" });
