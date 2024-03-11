434f8b9ee175ca236123d1e758da3eb559bfb1cd
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-11 11:29:06 -0600

    cv doanload complete flow with languages

--- src/controllers/downloadFile.js
+++ src/controllers/downloadFile.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const downloadFile = (req,res)=>{
     try {
-        const file = req.params.file;
+        const lang = req.query.lang; 
+        const file = lang === "en" ? "cvEnglish.pdf": "cvSpanish.pdf" ;
         const filePath = path.join(__dirname, '../../public', 'files', file);
         res.download(filePath);
     } catch (error) {
